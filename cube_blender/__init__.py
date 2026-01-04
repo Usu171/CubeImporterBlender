2ef4de0548a6b26699a4c20208b637b83f95b20f
Usu171 <usu171@proton.me>
2026-01-04 15:47:12 +0800

    chore: copyright, gitignore

--- cube_blender/__init__.py
+++ cube_blender/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025  Usu171
+# Copyright (C) 2026  Usu171
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
